61f5ca14e061d9a16373515922db01321e308653
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-10-17 06:44:30 +0545

    contact page is updated

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Contact() {
-  return <h1>This is a contact page</h1>;
+  return (
+    <h1>
+      This is a contact page
+      <h2>Browse to view contact.</h2>
+    </h1>
+  );
 }
 
 export default Contact;
