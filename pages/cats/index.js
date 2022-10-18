a642655f26713104069f9c428dd0de2dfba0c591
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-10-18 08:00:57 +0545

    nextjs data fetching technique

--- pages/cats/index.js
+++ pages/cats/index.js
@@ -1,47 +1,82 @@
 import axios from "axios";
-import React from "react";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
 
-function CatsList(props) {
-  console.log(props);
+// SSG
+export const getStaticProps = async () => {
+  const { data } = await axios("https://api.thecatapi.com/v1/breeds");
+  return {
+    props: { catsData: data },
+  };
+};
+
+// SSR
+// export const getServerSideProps = async () => {
+//   const { data } = await axios("https://api.thecatapi.com/v1/breeds");
+//   return {
+//     props: { catsData: data },
+//   };
+// };
+
+// ISR
+// export const getStaticProps = async () => {
+//   const { data } = await axios("https://api.thecatapi.com/v1/breeds");
+//   return {
+//     props: { catsData: data },
+//     revalidate: 20,
+//   };
+// };
+
+function CatsList({ catsData }) {
+  // CSR
+  //   const [catsData, setCatsData] = useState(null);
+  //   useEffect(() => {
+  //     async function fetchCatch() {
+  //       const { data } = await axios("https://api.thecatapi.com/v1/breeds");
+  //       setCatsData(data);
+  //     }
+  //     fetchCatch();
+  //   });
+
   return (
     <div>
       <h1>CAT&apos; Gallery</h1>
       <p>All about cats</p>
       <hr />
-      {/* {cats.length > 0 ? (
-        cats.map((cat) => (
+      {catsData?.length > 0 ? (
+        catsData.map((cat) => (
           <div key={cat.id}>
             <div>
               {cat.image && (
-                <img
-                  src={cat.image.url}
+                // <img
+                //   src={cat.image.url}
+                //   alt={cat.name}
+                //   width="200"
+                //   height="200"
+                // />
+                <Image
+                  src={cat.image?.url}
                   alt={cat.name}
-                  width="200"
-                  height="200"
+                  width="100%"
+                  height="100%"
+                  layout="responsive"
+                  objectFit="contain"
                 />
               )}
             </div>
             <div>
-              <h2>{cat.name}</h2>
-              <h2>{cat.temperament}</h2>
-              <h2>{cat.description}</h2>
+              <h1>{cat.name}</h1>
+              <h3>{cat.temperament}</h3>
+              <p>{cat.description}</p>
             </div>
           </div>
         ))
       ) : (
         <div>Loading...</div>
-      )} */}
+      )}
     </div>
   );
 }
-
-export const getStaticProps = async () => {
-  const data = await axios.get("https://api.thecatapi.com/v1/breeds");
-  console.log(data);
-  return {
-    props: 10,
-  };
-};
 
 export default CatsList;
 
